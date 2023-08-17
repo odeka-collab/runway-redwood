f2c29964084141b0ed1fc24c22daabc80d3ca665
Jimmy Pascual <jimmy@odeka.io>
2023-08-17 16:04:05 -0700

    animate runway

--- web/src/components/RunwayVisualizer/RunwayVisualizer.jsx
+++ web/src/components/RunwayVisualizer/RunwayVisualizer.jsx
@@ -1,4 +1,5 @@
-import Zdog from 'zdog'
+import anime from 'animejs'
+import Zdog, { TAU } from 'zdog'
 import Zfont from 'zfont'
 
 Zfont.init(Zdog)
@@ -8,13 +9,48 @@ const FONT = new Zdog.Font({
   src: '../../fonts/Ubuntu-Regular.ttf',
 })
 
+const DISPLAY = {
+  SEGMENT: {
+    BAR: {
+      WIDTH: 80,
+      HEIGHT: 240,
+      DEPTH: 60,
+      COLOR: {
+        FUNDED: {
+          frontFace: 'hsla(30, 89%, 52%, 1.0)',
+          rearFace: 'hsla(30, 68%, 52%, 1.0)',
+          leftFace: 'hsla(30, 68%, 52%, 1.0)',
+          rightFace: 'hsla(30, 68%, 52%, 1.0)',
+          topFace: 'hsla(30, 89%, 52%, 1.0)',
+          bottomFace: 'hsla(30, 68%, 52%, 1.0)',
+        },
+        PARTIAL: {
+          frontFace: 'hsla(30, 89%, 52%, 0.5)',
+          rearFace: 'hsla(30, 68%, 52%, 0.5)',
+          leftFace: 'hsla(30, 68%, 52%, 0.5)',
+          rightFace: 'hsla(30, 68%, 52%, 0.5)',
+          topFace: 'hsla(30, 89%, 52%, 0.5)',
+          bottomFace: 'hsla(30, 68%, 52%, 0.5)',
+        },
+      },
+    },
+    TEXT: {
+      MARGIN: 20,
+      FONT_SIZE: 24,
+      COLOR: {
+        FUNDED: '#66f',
+        PARTIAL: '#99f',
+      },
+    },
+  },
+}
+
 const RunwayVisualizer = ({ renderData, ...rest }) => {
   return (
     <section>
       {renderData?.months?.length > 0 && (
         <>
           <RunwayView months={renderData.months} />
-          <ZRunwayView months={renderData.months} />
         </>
       )}
       <pre>{JSON.stringify({ renderData, ...rest }, null, 2)}</pre>
@@ -23,101 +59,169 @@ const RunwayVisualizer = ({ renderData, ...rest }) => {
 }
 
 function RunwayView({ months }) {
-  return (
-    <div className="flex w-full">
-      {months?.map(({ label, debit, funded }) => {
-        return (
-          <div key={label}>
-            <div
-              className={`
-              h-72 w-24
-              border-collapse border-blue-900
-              ${
-                !debit || funded >= debit
-                  ? 'border-2 border-solid bg-blue-600'
-                  : 'border border-dashed bg-blue-400'
-              }
-              text-center
-            `
-                .replace(/\s+/g, ' ')
-                .trim()}
-            ></div>
-            <div className="text-center">{label}</div>
-          </div>
-        )
-      })}
-    </div>
-  )
-}
-
-function ZRunwayView({ months }) {
   const canvasRef = React.useRef()
-  useZDog({ canvasRef, months })
+  const runway = useRunway({
+    element: '.zdog-canvas',
+    canvasRef,
+    months,
+    dragRotate: true,
+  })
+  useAnime(runway)
   return <canvas ref={canvasRef} className="zdog-canvas" />
 }
 
-function useZDog({ canvasRef, months }) {
-  console.log('useZDog()')
+function useRunway({
+  canvasRef,
+  element,
+  months,
+  width,
+  height,
+  dragRotate = false,
+}) {
+  const [state, setState] = React.useState({})
+
   React.useEffect(() => {
-    console.log('useZDog.effect')
-    canvasRef.current.width = canvasRef.current.parentElement.clientWidth
-    const runway = renderRunway(months)
-
-    let animationId
-    animate()
-
-    function animate() {
-      runway.rotate.y += 0.03
-      runway.updateRenderGraph()
-      animationId = requestAnimationFrame(animate)
+    const stage = {
+      width: width || canvasRef.current.parentElement.clientWidth,
+      height: height || Math.floor(window.innerHeight * (2 / 3)),
     }
+
+    canvasRef.current.width = stage.width
+    canvasRef.current.height = stage.height
+
+    const illo = new Zdog.Illustration({
+      element,
+      dragRotate,
+    })
+
+    const root = new Zdog.Anchor()
+
+    illo.addChild(root)
+
+    const segments = months.map((month, i) => {
+      const segment = renderSegment(month)
+      segment.root.translate.x = i * DISPLAY.SEGMENT.BAR.WIDTH
+      root.addChild(segment.root)
+      return { root, ...segment }
+    })
+
+    let _animationId
+    if (dragRotate) {
+      _animate()
+    }
+
+    function _animate() {
+      // TODO: set drag state
+      // __must__ updateRenderGraph for drag rotation to work
+      illo.updateRenderGraph()
+      _animationId = requestAnimationFrame(_animate)
+    }
+
+    setState({ illo, root, segments, stage })
 
     return () => {
-      cancelAnimationFrame(animationId)
+      if (dragRotate) {
+        cancelAnimationFrame(_animationId)
+      }
     }
-  }, [canvasRef, months])
+  }, [element, canvasRef, months, width, height, dragRotate])
+
+  return state
 }
 
-function renderRunway({ months }) {
-  const illo = new Zdog.Illustration({
-    element: '.zdog-canvas',
+function useAnime(runway) {
+  React.useEffect(() => {
+    if (runway?.illo) {
+      const timeline = anime.timeline({
+        change: function () {
+          runway.illo.updateRenderGraph()
+        },
+      })
+
+      timeline.add({
+        targets: runway.root.rotate,
+        duration: runway.segments.length * (1000 - 700),
+        easing: 'easeInOutCubic',
+        x: -TAU / 30,
+        y: TAU / 18,
+      })
+
+      const rootState = {
+        translate: { x: 0 },
+      }
+
+      timeline.add(
+        {
+          targets: rootState.translate,
+          duration: runway.segments.length * (1000 - 700),
+          easing: 'easeInOutCubic',
+          x: -(runway.segments.length * DISPLAY.SEGMENT.BAR.WIDTH),
+          update: function () {
+            runway.root.translate.x = rootState.translate.x
+          },
+        },
+        500
+      )
+
+      const segmentTimeline = anime.timeline()
+
+      runway.segments.forEach((segment) => {
+        const segmentState = { height: 0 }
+
+        segmentTimeline.add(
+          {
+            targets: segmentState,
+            easing: 'easeInOutSine',
+            duration: 1000,
+            height: DISPLAY.SEGMENT.BAR.HEIGHT,
+            update: function () {
+              const prevBar = segment.bar
+              const height = anime.get(segmentState, 'height')
+              segment.bar = segment.bar.copy({
+                height,
+                translate: { y: -height / 2 },
+              })
+              prevBar.remove()
+            },
+          },
+          '-=700'
+        )
+      })
+    }
+  }, [runway])
+}
+
+function renderSegment({ label, debit, funded }) {
+  const bar = new Zdog.Box({
+    width: DISPLAY.SEGMENT.BAR.WIDTH,
+    height: 1,
+    depth: DISPLAY.SEGMENT.BAR.DEPTH,
+    ...(!debit || funded >= debit
+      ? DISPLAY.SEGMENT.BAR.COLOR.FUNDED
+      : DISPLAY.SEGMENT.BAR.COLOR.PARTIAL),
   })
 
-  illo.addChild(
-    // circle
-    new Zdog.Ellipse({
-      diameter: 80,
-      translate: { z: 40 },
-      stroke: 20,
-      color: '#636',
-    })
-  )
+  const text = new Zdog.Text({
+    font: FONT,
+    value: label,
+    fontSize: DISPLAY.SEGMENT.TEXT.FONT_SIZE,
+    fill: true,
+    color:
+      !debit || funded >= debit
+        ? DISPLAY.SEGMENT.TEXT.COLOR.FUNDED
+        : DISPLAY.SEGMENT.TEXT.COLOR.PARTIAL,
+    translate: {
+      x: -bar.width / 2,
+      y: bar.height / 2 + DISPLAY.SEGMENT.TEXT.MARGIN,
+      z: bar.depth / 2 + DISPLAY.SEGMENT.TEXT.MARGIN,
+    },
+  })
 
-  illo.addChild(
-    // square
-    new Zdog.Rect({
-      width: 80,
-      height: 80,
-      translate: { z: -40 },
-      stroke: 12,
-      color: '#E62',
-      fill: true,
-    })
-  )
+  const root = new Zdog.Anchor()
+  root.addChild(bar)
+  root.addChild(text)
 
-  illo.addChild(
-    // Create a text object
-    // This is just a Zdog.Shape object with a couple of extra parameters!
-    new Zdog.Text({
-      font: FONT,
-      value: 'Hey, Zdog!',
-      fontSize: 48,
-      fill: true,
-      color: '#66f',
-    })
-  )
-
-  return illo
+  return { root, bar, text }
 }
 
 export default RunwayVisualizer

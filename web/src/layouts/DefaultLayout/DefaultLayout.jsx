56dff81d6c83de61bc929088cf26d4ea84e195f9
Jimmy Pascual <jimmy@odeka.io>
2023-08-23 20:31:38 -0700

    reduce padding for better display on mobile

--- web/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -2,9 +2,9 @@ import Header from 'src/components/Header/Header'
 
 const DefaultLayout = ({ children }) => {
   return (
-    <div className="m-auto max-w-3xl p-2">
+    <div className="m-auto max-w-3xl p-1 pt-2 sm:pt-8">
       <Header />
-      <main className="relative p-2">{children}</main>
+      <main className="relative">{children}</main>
     </div>
   )
 }

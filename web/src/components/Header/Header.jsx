61d7687ca0955e4dbd496a713a4b1c05f26e8eef
Jimmy Pascual <jimmy@odeka.io>
2023-09-02 21:49:10 -0700

    added logo

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
+import logo from 'src/layouts/DefaultLayout/odeka_letterform_on_white.svg'
+
 const Header = () => {
   return (
-    <header className="flex flex-wrap items-center justify-between gap-4 pb-8">
-      <h1 className="flex-grow text-center text-4xl font-bold">
-        The Runway App
-      </h1>
+    <header className="flex flex-wrap justify-between gap-4 pb-8">
+      <img src={logo} alt="Odeka" width="120" />
+      <h1 className="text-2xl font-bold">Runway App</h1>
     </header>
   )
 }

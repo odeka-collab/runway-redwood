89788594606f67f93dcfc5e7140585398638b0ba
Jimmy Pascual <jimmy@odeka.io>
2023-09-09 19:04:05 -0700

    add bottom padding to layout

--- web/src/layouts/DefaultLayout/DefaultLayout.jsx
+++ web/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -2,7 +2,7 @@ import Header from 'src/components/Header/Header'
 
 const DefaultLayout = ({ children }) => {
   return (
-    <div className="m-auto max-w-3xl p-1 pt-2 sm:pt-8">
+    <div className="m-auto max-w-3xl p-1 py-2 sm:py-8">
       <Header />
       <main className="relative">{children}</main>
     </div>
